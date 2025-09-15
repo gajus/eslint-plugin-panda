353f0e51d4872c0c9b774b6914fcd0441203c549
Gajus <gajus@gajus.com>
2025-09-15 14:51:41 -0600

    docs: chore: add knip

---
+++ sandbox/eslint.config.mjs
@@ -0,0 +1,42 @@
+import eslint from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import panda from 'eslint-plugin-panda'
+import { defineConfig } from 'eslint/config';
+
+const compat = new FlatCompat()
+
+export default defineConfig({
+  files: ['**/*.ts', '**/*.tsx'],
+  ignores: [
+    '**/*.d.ts',
+    'styled-system',
+    // Ignore panda errors cause that's what we're here for
+    // 'src/App.tsx',
+  ],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...compat.config(reactHooks.configs.recommended),
+  ],
+  plugins: {
+    panda,
+    'react-refresh': reactRefresh,
+  },
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.es2020,
+    },
+  },
+  rules: {
+    ...panda.configs.recommended.rules,
+    'panda/no-margin-properties': 'warn',
+    'panda/no-physical-properties': 'error',
+    'panda/no-hardcoded-color': ['error', { noOpacity: true, whitelist: ['inherit'] }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+})
---
+++ sandbox/postcss.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: {
+    '@pandacss/dev/postcss': {},
+  },
+}
