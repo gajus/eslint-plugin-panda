c27adfc07a7de2acf99403c403fe9ba066d1c527
Gajus <gajus@gajus.com>
2025-09-15 16:25:49 -0600

    chore: include cjs files

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('./lib/types').Configuration} */
 module.exports = {
   '*': ['pnpm format'],
-  '**/*.{ts,tsx,js,jsx,json}': () => [
+  '**/*.{ts,tsx,cjs,js,jsx,json}': () => [
     'pnpm run lint:knip',
     'pnpm run test:vitest',
   ],
