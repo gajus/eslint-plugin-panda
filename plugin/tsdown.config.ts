bad59d8f19764a11197ce1ae81d791b2501bdce3
Gajus <gajus@gajus.com>
2025-09-15 13:54:24 -0600

    chore: migrate to tsdown

--- plugin/tsdown.config.ts
+++ plugin/tsdown.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+  clean: true,
+  dts: true,
   entry: ['src/index.ts', 'src/utils/worker.ts'],
   format: ['esm', 'cjs'],
+  minify: false,
+  report: true,
   shims: true,
+  sourcemap: true,
 })
