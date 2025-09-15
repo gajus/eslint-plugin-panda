ce447416b61646a2d7a882fb24e23f7987258638
Gajus <gajus@gajus.com>
2025-09-15 16:37:26 -0600

    chore: use build in lint-staged

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -2,6 +2,8 @@
 module.exports = {
   '*': ['pnpm format'],
   '**/*.{ts,tsx,cjs,js,jsx,json}': () => [
+    'pnpm run build',
+    'pnpm run lint:tsc',
     'pnpm run lint:knip',
     'pnpm run test:vitest',
   ],
